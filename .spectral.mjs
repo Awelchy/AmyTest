454f2fde5fc28097c3cba6d5223a7a6885703b7e
amy.welch@stoplight.io <amy.welch@stoplight.io>
2023-05-24 10:45:40 -0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMjUwNA/spectral.js?branch_slug=main&token=3b6e8694-cb21-455f-a10e-8548c14cdcfc";
+export default { extends: ruleset };
